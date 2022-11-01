d3fe45ae899c916caacdf44cbc05daf241c2809f
budarin.dmitriy2 <budarindmbd@gmail.com>
2022-11-02 02:21:21 +0300

    куча всего

--- internal/controller/handler.go
+++ internal/controller/handler.go
@@ -56,7 +56,7 @@ func (a *App) handler() {
 		if a.accessAdminChecker(obj.Message.FromID, obj.Message.PeerID, []string{RoleAdmin, RoleModerator, RoleNickolauyk}) {
 			if msg == "/settings" {
 				fmt.Println("/settings")
-				err := a.sendMsgBuilder(&obj, "/settings тест ci/cd 2")
+				err := a.sendMsgBuilder(&obj, "/settings тест ci/cd 3")
 				if err != nil {
 					return
 				}
