d6e9622bb8977cd2684bd3ab4a780a467229e1d1
budarin.dmitriy2 <budarindmbd@gmail.com>
2022-11-02 00:48:15 +0300

    куча всего

--- internal/repo/db/db.go
+++ internal/repo/db/db.go
@@ -4,10 +4,8 @@ import (
 	"bpzh_vk_bot/internal/config"
 	"cloud.google.com/go/firestore"
 	"context"
-	"fmt"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"google.golang.org/api/option"
-	"io/ioutil"
 )
 
 type Repo struct {
@@ -22,12 +20,12 @@ func NewRepo(FS *firestore.Client) *Repo {
 }
 
 func CreateFSConnections(cfg *config.DBConfig) (*firestore.Client, error) {
-	err := ioutil.WriteFile("fs.json", []byte(cfg.FSConf), 0777)
-	if err != nil {
-		fmt.Println(err)
-	}
+	//err := ioutil.WriteFile("fs.json", []byte(cfg.FSConf), 0777)
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
 
-	options := option.WithCredentialsFile("./fs.json")
+	options := option.WithCredentialsFile("./bot_fs.json")
 	client, err := firestore.NewClient(context.Background(), "bpzh-info", options)
 	if err != nil {
 		return nil, err
